921c806bc3027b860fcb9ccc0badcd0ca3d335e2
Giuseppe Crinò <giuscri@gmail.com>
2016-03-16 18:03:10 +0100

    Add test for unregistered users

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ describe('server', function() {
         server.listen(8080);
     });
 
-    it('should authenticate user `usr`', function(done) {
+    it('should recognize < \'usr\':md5(\'passwd\') >', function(done) {
         options = {
             hostname: 'localhost',
             port: 8080,
@@ -39,6 +39,38 @@ describe('server', function() {
             };
         });
         hash.end('passwd');
+    });
+
+    it('should NOT recognize < \'usr\':md5(\'p4zw0rd\') >', function(done) {
+        options = {
+            hostname: 'localhost',
+            port: 8080,
+            method: 'PUT',
+            path: '/',
+            headers: {'Content-Type': 'application/json'},
+        }
+        var req = http.request(options, function(res) {
+            var body = '';
+            res.on('data', function(chunk) {
+                body += chunk;
+            });
+            res.on('end', function() {
+                assert.deepEqual(JSON.parse(body), {valid: false});
+                done();
+            });
+        });
+        var hash = crypto.createHash('md5');
+        hash.on('readable', () => {
+            var data = hash.read();
+            if (data) {
+                auth_data = {
+                    username: 'usr',
+                    passwd_hash: data.toString('hex'),
+                };
+                req.end(JSON.stringify(auth_data));
+            };
+        });
+        hash.end('p4zw0rd');
     });
 
     after(function() {

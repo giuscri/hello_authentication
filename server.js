331aa1103c1d5984836d190cc7bb7f2f11402a54
Giuseppe Crinò <giuscri@gmail.com>
2016-03-16 18:02:47 +0100

    Make the server succeed the tests

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 var http = require('http');
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./main.db');
+
 var server = http.createServer(function(req, res) {
-    res.end(JSON.stringify({valid: false}));
+    if (req.method === 'PUT' && req.url.match(/^\/$/)) {
+        var body = '';
+        req.on('data', (chunk) => body += chunk);
+        req.on('end', () => {
+            var parsed_body = JSON.parse(body);
+            var query = `select * from main where username='${parsed_body['username']}'`;
+            db.get(query, function(err, row) {
+                if (err) { /* Do nothing. Yeah!, it's just a test man!! */}
+
+                if (row['passwd_hash'] === parsed_body['passwd_hash']) {
+                    return res.end(JSON.stringify({valid: true}));
+                }
+
+                return res.end(JSON.stringify({valid: false}));
+            });
+        });
+    }
 });
 
 module.exports = server;
